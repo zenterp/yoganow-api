a55685239e954b8521d38fc43804040a4d15e5ab
Steven Zeiler <zeiler.steven@gmail.com>
2013-04-25 18:10:28 -0700

    add gems and create local postgresql database.

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+# The usual gems
+gem 'rails', '3.2.13'
+gem 'pg'
+gem 'thin'
+gem 'rails_admin'
+gem "devise"
+gem "active_model_serializers"
+
+# App-specific
+gem 'geocoder'
+gem 'groupdate'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'debugger'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoganowApi::Application.config.secret_token = '48dc59b7f049429d84caea2746c114f92fbc19d1e1c3b9bd102cbfccf2b1ed0873c13afe16c68a6203cc4120bc6452ea1f59ecc2fc11809ee1b16f91bff96ac9'
