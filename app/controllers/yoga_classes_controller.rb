0788f8617161a09af36888485864afb1b308752f
Steven Zeiler <zeiler.steven@gmail.com>
2013-04-30 13:12:39 -0500

    params yoga_studio_id

--- app/controllers/yoga_classes_controller.rb
+++ app/controllers/yoga_classes_controller.rb
@@ -1,6 +1,6 @@
 class YogaClassesController < ApplicationController
   def index
-    studio = YogaStudio.find(params[:yoga_studio])
+    studio = YogaStudio.find(params[:yoga_studio_id])
 
 	render json: studio.yoga_classes
   end
