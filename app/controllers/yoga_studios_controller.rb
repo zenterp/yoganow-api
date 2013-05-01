cade461b77559d4e9c457fcc3279853304655c6f
Steven Zeiler <zeiler.steven@gmail.com>
2013-04-30 22:42:52 -0700

    no root in serializer

--- app/controllers/yoga_studios_controller.rb
+++ app/controllers/yoga_studios_controller.rb
@@ -1,5 +1,7 @@
 class YogaStudiosController < ApplicationController
   def index
+  	ActiveRecord::Base.include_root_in_json = true
+
     location = params["location"] || "berkeley"
     
 	render json: YogaStudio.near(location)
