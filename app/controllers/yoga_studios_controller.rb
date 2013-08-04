88fdc9d76e151ea84d42926f9579eb6945e4f17b
Steven Zeiler <zeiler.steven@gmail.com>
2013-08-04 13:54:59 -0700

    render lat lng.

--- app/controllers/yoga_studios_controller.rb
+++ app/controllers/yoga_studios_controller.rb
@@ -1,6 +1,6 @@
 class YogaStudiosController < ApplicationController
   def index
   	ActiveRecord::Base.include_root_in_json = false
-	  render json: YogaStudio.near(params[:lat], params[:lng])
+	  render json: { lat: params[:lat], lng: params[:lng], studios: YogaStudio.near(params[:lat], params[:lng]) }.to_json
   end
 end
